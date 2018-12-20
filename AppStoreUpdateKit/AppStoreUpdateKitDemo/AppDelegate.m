28abed5259deddca3c294ab1cc26edffa7ecdd1d
HsiangHo <ho_cheung@qq.com>
2018-12-21 01:25:53 +0800

    update demo code

--- AppStoreUpdateKit/AppStoreUpdateKitDemo/AppDelegate.m
+++ AppStoreUpdateKit/AppStoreUpdateKitDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AppStoreUpdateKit/AppStoreUpdateKit.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,30 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    AppStoreUpdateAppObject *appObj = [[AppStoreUpdateAppObject alloc] initWithAppName:@"Kaka" withAppIcon:[NSImage imageNamed:@"cat"] withCurrentVersion:@"1.0.9" withProductID:@"1434172933"];
+    [[AppStoreUpdateManager sharedManager] checkAppUpdateAsync:appObj withCompletionBlock:^(BOOL rslt, AppStoreUpdateAppObject * _Nonnull AppObj) {
+        if (rslt && ![[AppStoreUpdateManager sharedManager] isCurrentNewVersionSkipped:appObj]) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [[AppStoreUpdateManager sharedManager] requestAppUpdateWindow:appObj withCompletionCallback:^(AppUpdateWindowResult rslt, AppStoreUpdateAppObject * _Nonnull AppObj) {
+                    switch (rslt) {
+                        case AppUpdateWindowResultUpdate:
+                            [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"macappstore://itunes.apple.com/app/id%@",[AppObj productID]]]];
+                            break;
+                            
+                        case AppUpdateWindowResultSkip:
+                            [[AppStoreUpdateManager sharedManager] skipCurrentNewVersion:AppObj];
+                            break;
+                            
+                        case AppUpdateWindowResultLater:
+                            break;
+                            
+                        default:
+                            break;
+                    }
+                }];
+            });
+        }
+    }];
 }
 
 
